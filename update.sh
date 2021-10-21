94e7e675c6e3604ed5a4c35783e69d889746f15e
Suomifin <76266692+Suomifin@users.noreply.github.com>
2021-10-21 11:23:32 +0300

    Update Sh script

---
+++ update.sh
@@ -0,0 +1,15 @@
+echo "Updating..."
+
+sudo apt update
+sudo apt upgrader -y
+
+echo "Server is updated!"
+
+
+echo ""
+echo ""
+
+echo "==============================="
+echo "Script by: Suomifin"
+echo "Last update: 21.10.2021"
+echo "==============================="
