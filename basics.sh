9396213bbbaa79a1634d99a67dc80f80db7fa1d4
Suomifin <76266692+Suomifin@users.noreply.github.com>
2022-06-03 08:25:31 +0300

    Update basics.sh

--- basics.sh
+++ basics.sh
@@ -1,4 +1,17 @@
+echo 'Server basics installitation.'
+
+echo 'Updates and upgrades'
+apt update
+apt upgrade -y
+
+echo 'Installing nginx'
 apt install nginx -y
 
-apt install curl
-apt install sudo
+echo 'Installing php 8.0'
+curl https://packages.sury.org/php/apt.gpg -o /etc/apt/trusted.gpg.d/php.gpg
+echo "deb https://packages.sury.org/php/ buster main" | tee /etc/apt/sources.list.d/php.list
+apt update
+apt install -y php8.0 php8.0-{cli,common,gd,mysql,mbstring,bcmath,xml,fpm,curl,zip}
+
+systemctl enable php8.0-fpm
+systemctl start php8.0-fpm
