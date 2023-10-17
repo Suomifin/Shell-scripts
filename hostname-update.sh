1cffd2ad793d7df7173e5f27b5f2a2d52b2b3507
Suomifin <76266692+Suomifin@users.noreply.github.com>
2023-10-17 11:35:38 +0300

    updated

--- hostname-update.sh
+++ hostname-update.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
+echo "Script made by suomifin || suomifin.eu"
 
-# Ask for the new hostname
 read -p "Enter the new hostname (e.g. debian-vm101): " new_hostname
 
-# Update /etc/hosts
 sudo sed -i "s/127.0.1.1.*/127.0.1.1\t$new_hostname/g" /etc/hosts
 
-# Update /etc/hostname
 echo "$new_hostname" | sudo tee /etc/hostname
 
-# Notify the user
 echo "Hostname has been updated to $new_hostname"
 
-# Request a reboot to apply the changes
 read -p "Do you want to reboot now? (y/n): " reboot_choice
 
 if [ "$reboot_choice" == "y" ]; then
@@ -20,3 +16,4 @@ if [ "$reboot_choice" == "y" ]; then
 else
     echo "Please remember to reboot your system to apply the changes."
 fi
+
